61c029c95a788bb0acb2711ad32d66e3b0eb256e
Alex Koskovich <ZVNexus@outlook.com>
2020-01-14 09:47:25 -0500

    Update Solus setup script.
    - Show packages in list format and sort alphabetically.
    - Add VIM
    - Switch to Google's URL for Repo
    - Move Repo to /usr/bin (Local hierarchy doesn't exist in Solus).
    
    // TODO
    - Add vboot-utils once I submit package into the Solus repository.

--- setup/solus.sh
+++ setup/solus.sh
@@ -1,26 +1,42 @@
 #!/usr/bin/env bash
 
-# Copyright (C) 2019 ZVNexus
+# Copyright (C) 2019-2020 ZVNexus
 # SPDX-License-Identifier: GPL-3.0-only
 
 # Script to setup an AOSP build environment on Solus
 
 sudo eopkg it -c system.devel
-sudo eopkg it openjdk-8-devel curl-devel git gnupg gperf libgcc-32bit libxslt-devel lzop ncurses-32bit-devel ncurses-devel readline-32bit-devel rsync schedtool sdl1-devel squashfs-tools unzip wxwidgets-devel zip zlib-32bit-devel lzip
+sudo eopkg it \
+    android-tools \
+    curl-devel \
+    git \
+    gnupg \
+    gperf \
+    libgcc-32bit \
+    libxslt-devel \
+    lzip \
+    lzop \
+    ncurses-32bit-devel \
+    ncurses-devel \
+    openjdk-8-devel \
+    readline-32bit-devel \
+    rsync \
+    schedtool \
+    sdl1-devel \
+    squashfs-tools \
+    unzip \
+    vim \
+    wxwidgets-devel \
+    zip \
+    zlib-32bit-devel
 
-# ADB/Fastboot
-sudo eopkg bi --ignore-safety https://raw.githubusercontent.com/solus-project/3rd-party/master/programming/tools/android-tools/pspec.xml
-sudo eopkg it android-tools*.eopkg;sudo rm android-tools*.eopkg
-
-# udev rules
-echo "Setting up udev rules for adb!"
+# Setup udev rules for ADB.
+echo "Setting up udev rules for ADB!"
 sudo curl --create-dirs -L -o /etc/udev/rules.d/51-android.rules -O -L https://raw.githubusercontent.com/M0Rf30/android-udev-rules/master/51-android.rules
 sudo chmod 644 /etc/udev/rules.d/51-android.rules
 sudo chown root /etc/udev/rules.d/51-android.rules
 sudo usysconf run -f
 
 echo "Installing repo"
-sudo curl --create-dirs -L -o /usr/local/bin/repo -O -L https://github.com/akhilnarang/repo/raw/master/repo
-sudo chmod a+x /usr/local/bin/repo
-
-echo "You are now ready to build Android!"
+sudo curl --create-dirs -L -o /usr/bin/repo -O -L https://storage.googleapis.com/git-repo-downloads/repo
+sudo chmod a+x /usr/bin/repo
